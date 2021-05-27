b63afa802762b4e9bcfc0096efee5a6d2056abbb
rishabhx64 <rishabharora1780@gmail.com>
2021-05-28 04:16:06 +0530

    add task handler

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,18 +1,28 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {connect} from 'react-redux';
 import Task from './Task';
 import '../styles/components.css';
 
 
+const changeTitle = () => {
+
+};
 const Stage = (props) => {
     const {stage} = props;
-
+    const [addingTask, toggleState] = useState(false);
     return(
         <div className="Stage">
-            <div className="Stage-Title">{stage.title}</div>
+            <div className="Stage-Title" onClick={changeTitle()} >{stage.title}</div>
             {stage.tasks && stage.tasks.map((taskId, index) => {
                 return <Task stageId={stage.id} taskId={taskId} index={index} />
             })}
+            {addingTask ? (
+                    console.log(1)
+                    ) : (
+                    <div className="Toggle-Add-Stage" onClick={() => toggleState(!addingTask)}>
+                        Add a card
+                    </div>
+                    )}
         </div>
     );
 }
